e47f29aee555f0faa04d6d2a7a08fe1601343a46
Sebastian Riquelme <sebarsalbo@gmail.com>
2022-11-18 19:14:00 -0300

    creacion de documentos lista

--- tarea_3/functions/createDocuments.js
+++ tarea_3/functions/createDocuments.js
@@ -3,9 +3,9 @@
 //of the file to be created
 
 exports.createDocuments = (path, name, content) => {
-     //import fs module
-     const fs = require("fs");
-	fs.writeFile("./documents/" + name, content, (err) => {
+	//import fs module
+	const fs = require("fs");
+	fs.writeFile(path + name, content, (err) => {
 		if (err) throw err;
 		console.log("The file has been saved!");
 	});
