0d363c9ba8f671661d8cc95365690d45ef5b19fc
Sebastian Riquelme <sebarsalbo@gmail.com>
2022-11-28 00:58:16 -0300

    API lista

--- tarea_3/API/app.js
+++ tarea_3/API/app.js
@@ -1,12 +1,14 @@
-const express = require("express");
-const mysql = require("mysql");
+import express from "express";
+import mysql from "mysql";
+import bodyParser from "body-parser";
 
+//====================DATABASE===============
 const con = mysql.createConnection({
-	host: "mariadb",
+	host: "127.0.0.1",
 	user: "root",
 	port: 3306,
 	password: "rootpass",
-	database: "distribuidos",
+	database: "tarea3",
 	connectTimeout: 99999999,
 	acquireTimeout: 99999999,
 	waitForConnections: true,
@@ -17,21 +19,63 @@ con.connect(function (err) {
 	if (err) throw err;
 	console.log("Connected to DB!");
 });
+//=========================================
 
 const app = express();
 
 app.use(express.json());
 
-//ruta que recibe una palabra y la busca en la base de datos
-app.get("/api/word/:word", (req, res) => {
-     const word = req.params.word;
-     con.query(`SELECT * FROM words WHERE word = '${word}'`, function (err, result, fields) {
-          if (err) throw err;
-          res.send(result);
-     });
+//Para leer desde form HTML
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Renderiza login.handlebars en la ruta principal
+app.get("/", (req, res) => {
+	res.json({
+		message: "Bienvenido a la API del proyecto!",
+	});
 });
 
+app.post("/search", (req, res) => {
+	console.log(req.body);
+	const palabra = req.body.palabra;
+
+	try {
+		var sql = `SELECT * FROM COUNT WHERE palabra='${palabra}'`;
+		con.query(sql, function (err, result) {
+			if (err) {
+				//throw err;
+				console.log(err);
+				//res.send(err);
+			} else {
+				//Si la query es correcta
+				var result_count = result;
+				const id_documento = result_count[0].id_documento;
+				try {
+					var sql = `SELECT * FROM TITULOS WHERE id=${id_documento}`;
+					con.query(sql, function (err, result) {
+						if (err) {
+							//throw err;
+							console.log(err);
+							//res.send(err);
+						} else {
+							//Si la query es correcta
+
+							console.log(result);
+							res.json({
+								resultados: result,
+							});
+						}
+					});
+				} catch (error) {
+					callback(error, null);
+				}
+			}
+		});
+	} catch (error) {
+		callback(error, null);
+	}
+});
 
 app.listen(3000, () => {
-	console.log("\nServer running on port 3000\n");
+	console.log("Server express-handlebars corriendo en puerto: 3000");
 });
--- tarea_3/meter_a_db/output_hadoop/conection.py
+++ tarea_3/meter_a_db/output_hadoop/conection.py
@@ -2,14 +2,14 @@ import mysql.connector
 
 
 mydb = mysql.connector.connect(
-  host="localhost",
+  host="127.0.0.1",
   user="root",
-  passwd="",
-  database="datos"
+  passwd="rootpass",
+  database="tarea3"
 )
 mycursor = mydb.cursor()
 
-for renglon in open ('/home/bastian/tareas_sistemas_distribuidos/tarea_3/meter_a_db/output_hadoop/output1.txt'):
+for renglon in open ('/home/seba/Escritorio/Estudio/sistemas_distribuidos/tareas/tarea_3/meter_a_db/output_hadoop/output1.txt'):
     partes = renglon.split()
     #print(partes)
     #print(partes[0])
@@ -18,7 +18,7 @@ for renglon in open ('/home/bastian/tareas_sistemas_distribuidos/tarea_3/meter_a
     id_documento = partes[2][0]
     palabra = partes[0]
     contador = partes[1]
-    sql = "INSERT INTO datos.COUNT(id_documento,palabra,contador)VALUES(%s,%s,%s)"
+    sql = "INSERT INTO tarea3.COUNT(id_documento,palabra,contador)VALUES(%s,%s,%s)"
     val = (id_documento,palabra,contador)
     mycursor.execute(sql,val)
     mydb.commit()
